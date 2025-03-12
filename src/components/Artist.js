df846fb330b6198a084322106ad4c65b6f2f426f
Wing <eamonn.kearns@so-4pt.net>
2025-03-12 02:05:41 +0000

    Started working on axios

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,32 +1,48 @@
-import { useState, useId } from 'react';
+import { useState, useId, useEffect} from 'react';
+import axios from 'axios';
+const controller = new AbortController();
+let changeEvents = 0;
 
-function SearchBar({searchText, onSearchTextChange})
+function handleChange(e)
 {
-    const searchInputId = useId();
-    return (<div className="App-Artist-Searchbar">
-        <form>
-            <div className="SearchBarElement">
-                <label htmlFor={searchInputId}>Search</label>
-            </div>
-            <div className="SearchBarElement">
-                <input type="text"
-                       id={searchInputId}
-                       value={searchText}
-                       onChange={(e)=>onSearchTextChange(e.target.value)}/>
-            </div>
-        </form>
-    </div>);
+    console.log(e);
 }
 
-function ArtistsTable({artists})
+function SearchBar()
 {
     const [searchText, setSearchText] = useState('');
-    return (
-        <div className="App-Artitsts-Container">
-            <SearchBar searchText={searchText} onSearchTextChange={setSearchText}/>
+    const [searchResults, setSearchResults] = useState([]);
+    const searchInputId = useId();
 
+
+
+    return (
+        <div>
+            <div className="App-Artists-SearchBar">
+                <div className="SearchBarElement">
+                    <label htmlFor={searchInputId}>Search</label>
+                </div>
+                <div className="SearchBarElement">
+                    <input type="text"
+                           id={searchInputId}
+                           value={searchText}
+                           onChange={(e)=>{
+                               setSearchText(e.target.value);
+                               axios.get(`http://localhost:3000/artists/startingWith/${e.target.value}`).then(res=>{
+                                   res.data.forEach((band)=>{
+                                       console.log(band);
+                                   });
+
+                               });
+                           }}/>
+                </div>
+            </div>
+            <div>
+
+            </div>
         </div>
     );
 }
 
-export default ArtistsTable;
+
+export default SearchBar;
